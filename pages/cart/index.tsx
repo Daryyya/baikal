081c4c68ce767a0776a4064859f750ce70ef0793
Daryyya <darya9841402670@gmail.com>
2023-01-10 15:13:44 +0300

    feat: add table styles

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { observer } from "mobx-react";
 import { myOrderFormData } from "../../store/orderFormData";
 import Table from "../../component/Table";
-import Button from "../../kit/Button";
+import {Style, Wrapper, ButtonsWrapper, StyledButton, StyledLink, Title} from '../../styles/cart';
 
 const columns = [
-  { Header: "1", accessor: "id" },
+  { Header: " ", accessor: "id" },
   { Header: "Кол-во", accessor: "amount" },
   { Header: "Общая масса нетто, кг", accessor: "netWeight" },
   { Header: "Общая масса брутто, кг", accessor: "grossWeight" },
@@ -18,16 +18,19 @@ const Cart = observer(() => {
   const order = myOrderFormData.getState();
   
   return (
-    <div>
-      <p>Добавленная мебель({order.length})</p>
-      <Table data={order} columns={columns} />
-      <Button variant="blue" type="button">
+    <Wrapper>
+      <Title>Добавленная мебель({order.length})</Title>
+      <Style><Table data={order} columns={columns} /></Style>
+
+      <ButtonsWrapper>
+      <StyledLink variant="blue" page="/order">
         Добавить
-      </Button>
-      <Button variant="blue" type="button">
+      </StyledLink>
+      <StyledButton variant="blue" type="button">
         Рассчитать
-      </Button>
-    </div>
+      </StyledButton>
+      </ButtonsWrapper>
+    </Wrapper>
   );
 });
 
