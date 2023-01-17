09e28e6374160d36e921265d0e5a6bb1cc15fcfb
Daryyya <darya9841402670@gmail.com>
2023-01-17 23:37:05 +0300

    fix: change home form name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import HomeForm from "../component/Form";
+import HomeForm from "../component/HomeForm";
 import backgroundImage from '../public/img/bg-home.png';
 import { Title, Wrapper, Container, BackgroundImage } from "../styles/home";
 // import { Inter } from "@next/font/google";
